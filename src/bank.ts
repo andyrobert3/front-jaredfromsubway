2b55699ff28d442368fd3a471faf4aaed1d85c63
Matthew <alexandermatthew12@gmail.com>
2023-10-16 21:53:29 +0800

    feat: add interface with bank contract

---
+++ src/bank.ts
@@ -0,0 +1,49 @@
+import {
+  Address,
+  decodeAbiParameters,
+  encodeAbiParameters,
+  getFunctionSelector,
+  parseAbiItem,
+} from "viem";
+import { HexString } from "./alchemy";
+import { ethers, providers } from "ethers";
+
+export const BANK_CONTRACT_ADDRESS =
+  "0xD76465f2026F2ed2BC0016608E8354A99D8d60aC";
+export const withdrawFnAbi = `function withdraw(bytes memory data, bytes memory signature, address receiver) public`;
+export const WITHDRAW_FN_GAS_LIMIT = BigInt(67_500);
+
+const withdrawFnAbiItem = parseAbiItem(withdrawFnAbi);
+export const withdrawFnSelector = getFunctionSelector(withdrawFnAbiItem);
+
+/**
+ * Withdraw function can be seen here:
+ * https://polygonscan.com/address/0xd76465f2026f2ed2bc0016608e8354a99d8d60ac#code
+ *
+ * Function signature
+ * function withdraw(bytes memory data, bytes memory signature, address payable receiver)
+ */
+export const decodeWithdrawFnParameters = (txData: HexString) => {
+  // Remove the function signature from the tx data
+  const inputData: HexString = `0x${txData.slice(10)}`;
+  return decodeAbiParameters(withdrawFnAbiItem.inputs, inputData);
+};
+
+export const encodeWithdrawFnParameters = (
+  data: HexString,
+  signature: HexString,
+  receiver: Address,
+): HexString => {
+  const inputData = encodeAbiParameters(withdrawFnAbiItem.inputs, [
+    data,
+    signature,
+    receiver,
+  ]);
+  // Add the function signature back to the tx data
+  return `${withdrawFnSelector}${inputData.slice(2)}`;
+};
+
+// Ethers related types
+export const getEthersBankContract = (provider: providers.Provider) => {
+  return new ethers.Contract(BANK_CONTRACT_ADDRESS, [withdrawFnAbi], provider);
+};
