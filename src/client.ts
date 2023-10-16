601f1e1a8cb1f36b89adc68909dd2b3de08320f2
Matthew <alexandermatthew12@gmail.com>
2023-10-16 21:54:45 +0800

    chore: introduce rpc client related funcs

---
+++ src/client.ts
@@ -0,0 +1,93 @@
+import { privateKeyToAccount } from "viem/accounts";
+import {
+  createPublicClient,
+  createWalletClient,
+  http,
+  WalletClient,
+  webSocket,
+} from "viem";
+import { polygon } from "viem/chains";
+import { HexString } from "./alchemy";
+import { RequireAtLeastOne } from "alchemy-sdk";
+
+export const getWalletClient = (
+  privateKey: HexString,
+  {
+    httpRpcUrl,
+    wsRpcUrl,
+  }: RequireAtLeastOne<{ httpRpcUrl?: string; wsRpcUrl?: string }>,
+) => {
+  return createWalletClient({
+    account: privateKeyToAccount(privateKey),
+    chain: polygon,
+    transport: httpRpcUrl ? http(httpRpcUrl) : webSocket(wsRpcUrl),
+  });
+};
+
+export const getPublicClient = ({
+  httpRpcUrl,
+  wsRpcUrl,
+}: RequireAtLeastOne<{ httpRpcUrl?: string; wsRpcUrl?: string }>) => {
+  return createPublicClient({
+    chain: polygon,
+    transport: httpRpcUrl ? http(httpRpcUrl) : webSocket(wsRpcUrl),
+  });
+};
+
+// Create clients for Alchemy, Infura and QuickNode
+let alchemyWalletClient: WalletClient;
+let infuraWalletClient: WalletClient;
+let quickNodeWalletClient: WalletClient;
+
+const getAlchemyWalletClient = () => {
+  if (!alchemyWalletClient) {
+    alchemyWalletClient = getWalletClient(
+      process.env.PRIVATE_KEY as HexString,
+      {
+        wsRpcUrl: process.env.ALCHEMY_WEBSOCKET_RPC_URL as string,
+      },
+    );
+  }
+  return alchemyWalletClient;
+};
+
+const getInfuraWalletClient = () => {
+  if (!infuraWalletClient) {
+    infuraWalletClient = getWalletClient(process.env.PRIVATE_KEY as HexString, {
+      httpRpcUrl: process.env.INFURA_RPC_URL as string,
+    });
+  }
+  return infuraWalletClient;
+};
+
+const getInfuraPublicClient = () => {
+  return getPublicClient({
+    httpRpcUrl: process.env.INFURA_RPC_URL as string,
+  });
+};
+
+const getQuickNodeWalletClient = () => {
+  if (!quickNodeWalletClient) {
+    quickNodeWalletClient = getWalletClient(
+      process.env.PRIVATE_KEY as HexString,
+      {
+        wsRpcUrl: process.env.QUICKNODE_WEBSOCKET_RPC_URL_1 as string,
+      },
+    );
+  }
+  return quickNodeWalletClient;
+};
+
+const getQuickNodePublicClient = () => {
+  return getPublicClient({
+    httpRpcUrl: process.env.QUICKNODE_WEBSOCKET_RPC_URL_1 as string,
+  });
+};
+
+export {
+  getAlchemyWalletClient,
+  getInfuraWalletClient,
+  getQuickNodeWalletClient,
+  getInfuraPublicClient,
+  getQuickNodePublicClient,
+};
